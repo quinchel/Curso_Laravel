343967cc03f8c61afc76c0ba196e655b5af8f033
quinchel <lbeto_q@hotmail.com>
2019-08-27 14:48:15 -0500

    Trabajo con Seeders y Model Factory

---
+++ database/factories/PermisoFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+use App\Models\Permiso;
+
+$factory->define(Permiso::class, function (Faker $faker) {
+    return [
+        'nombre' => $faker->word,
+        'slug' => $faker->word,
+    ];
+});
